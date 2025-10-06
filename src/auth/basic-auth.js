062a5dcfd0222d57ec6e7f473ffc0e3bbafdb1d1
Avinav <137401496+avinav456@users.noreply.github.com>
2025-10-06 08:24:30 +0000

    added authentications files

--- src/auth/basic-auth.js
+++ src/auth/basic-auth.js
@@ -4,18 +4,22 @@
 // https://github.com/http-auth/http-auth-passport
 
 const auth = require('http-auth');
-const passport = require('passport');
+// const passport = require('passport');
 const authPassport = require('http-auth-passport');
 const logger = require('../logger');
 
-// We expect HTPASSWD_FILE to be defined.
+// --- NEW: Import the custom authorize middleware ---
+const authorize = require('./auth-middleware');
+
+// We expect HTPASSWD_FILE to be defined (points to .htpasswd file)
 if (!process.env.HTPASSWD_FILE) {
   throw new Error('missing expected env var: HTPASSWD_FILE');
 }
 
-// Log that we're using Basic Auth
+// Log that we're using Basic Auth for authentication
 logger.info('Using HTTP Basic Auth for auth');
 
+// Define the Basic Auth strategy for Passport
 module.exports.strategy = () =>
   // For our Passport authentication strategy, we'll look for a
   // username/password pair in the Authorization header.
@@ -25,4 +29,10 @@ module.exports.strategy = () =>
     })
   );
 
-module.exports.authenticate = () => passport.authenticate('http', { session: false });
+// --- UPDATED: use our custom authorize middleware ---
+// Previously we had:
+// module.exports.authenticate = () => passport.authenticate('http', { session: false });
+//
+// Now we delegate authorization to our custom middleware so the user's email
+// gets hashed for privacy and stored on req.user
+module.exports.authenticate = () => authorize('http');
--- src/auth/cognito.js
+++ src/auth/cognito.js
@@ -4,12 +4,13 @@
 // Identity Token provided by Cognito. The token will be
 // parsed from the Authorization header (i.e., Bearer Token).
 
-const passport = require('passport');
-
+// const passport = require('passport');
 const BearerStrategy = require('passport-http-bearer').Strategy;
 const { CognitoJwtVerifier } = require('aws-jwt-verify');
-
 const logger = require('../logger');
+
+// --- NEW: import our authorize middleware ---
+const authorize = require('./auth-middleware');
 
 // We expect AWS_COGNITO_POOL_ID and AWS_COGNITO_CLIENT_ID to be defined.
 if (!(process.env.AWS_COGNITO_POOL_ID && process.env.AWS_COGNITO_CLIENT_ID)) {
@@ -31,8 +32,6 @@ const jwtVerifier = CognitoJwtVerifier.create({
 
 // At startup, download and cache the public keys (JWKS) we need in order to
 // verify our Cognito JWTs, see https://auth0.com/docs/secure/tokens/json-web-tokens/json-web-key-sets
-// You can try this yourself using:
-// curl https://cognito-idp.us-east-1.amazonaws.com/<user-pool-id>/.well-known/jwks.json
 jwtVerifier
   .hydrate()
   .then(() => {
@@ -42,16 +41,15 @@ jwtVerifier
     logger.error({ err }, 'Unable to cache Cognito JWKS');
   });
 
+// Define our Passport strategy for Cognito bearer tokens
 module.exports.strategy = () =>
-  // For our Passport authentication strategy, we'll look for the Bearer Token
-  // in the Authorization header, then verify that with our Cognito JWT Verifier.
   new BearerStrategy(async (token, done) => {
     try {
-      // Verify this JWT
+      // Verify this JWT using Cognito
       const user = await jwtVerifier.verify(token);
       logger.debug({ user }, 'verified user token');
 
-      // Create a user, but only bother with their email
+      // Only pass their email forward to the middleware
       done(null, user.email);
     } catch (err) {
       logger.error({ err, token }, 'could not verify token');
@@ -59,4 +57,9 @@ module.exports.strategy = () =>
     }
   });
 
-module.exports.authenticate = () => passport.authenticate('bearer', { session: false });
+// --- UPDATED: use our custom authorize middleware ---
+// Previously we defined authenticate() like this:
+// module.exports.authenticate = () => passport.authenticate('bearer', { session: false });
+//
+// Now we delegate to authorize('bearer') so it will hash the user's email
+module.exports.authenticate = () => authorize('bearer');
