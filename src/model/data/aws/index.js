02236f94deb3e982fcc4d5f964e8f546d4f57120
Avinav <avinav@myseneca.ca>
2025-12-10 22:48:24 -0500

    Fix: Indentation error

--- src/model/data/aws/index.js
+++ src/model/data/aws/index.js
@@ -13,9 +13,7 @@ const { PutCommand, GetCommand, QueryCommand, DeleteCommand  } = require('@aws-s
 const { s3Client, s3BucketName } = require('./s3Client');
 const logger = require('../../../logger');
 
-// XXX: temporary use of memory-db until we add DynamoDB
-// const MemoryDB = require('../memory/memory-db');
-// const metadata = new MemoryDB();
+
 
 // Convert a stream of data into a Buffer
 const streamToBuffer = (stream) =>
@@ -26,19 +24,7 @@ const streamToBuffer = (stream) =>
     stream.on('end', () => resolve(Buffer.concat(chunks)));
   });
 
-// // Write fragment METADATA to memory (NOT data to S3!)
-// async function writeFragment(fragment) {
-//   try {
-//     // Store fragment metadata in memory (as JSON string)
-//     const serialized = JSON.stringify(fragment);
-//     await metadata.put(fragment.ownerId, fragment.id, serialized);
 
-//     logger.debug({ fragmentId: fragment.id }, 'Fragment metadata written');
-//   } catch (err) {
-//     logger.error({ err, fragmentId: fragment.id }, 'Error writing fragment metadata');
-//     throw err;
-//   }
-// }
 
 
 // Writes a fragment to DynamoDB. Returns a Promise.
@@ -60,11 +46,6 @@ function writeFragment(fragment) {
   }
 }
 
-// // Read a fragment's metadata from memory
-// async function readFragment(ownerId, id) {
-//   const serialized = await metadata.get(ownerId, id);
-//   return typeof serialized === 'string' ? JSON.parse(serialized) : serialized;
-// }
 
 
 
@@ -133,16 +114,7 @@ async function writeFragmentData(ownerId, id, buffer) {
   }
 }
 
-// // Get a list of fragment ids/objects for the given user from memory
-// async function listFragments(ownerId, expand = false) {
-//   const fragments = await metadata.query(ownerId);
 
-//   if (expand || !fragments) {
-//     return fragments;
-//   }
-
-//   return fragments.map((fragment) => JSON.parse(fragment).id);
-// }
 
 // Get a list of fragments, either ids-only, or full Objects, for the given user.
 // Returns a Promise<Array<Fragment>|Array<string>|undefined>
@@ -184,29 +156,6 @@ async function listFragments(ownerId, expand = false) {
   }
 }
 
-// // Delete a fragment's metadata from memory and data from S3
-// async function deleteFragment(ownerId, id) {
-//   const params = {
-//     Bucket: s3BucketName,
-//     Key: `${ownerId}/${id}`,
-//   };
-
-//   const command = new DeleteObjectCommand(params);
-
-//   try {
-//     // Delete metadata from memory
-//     await metadata.del(ownerId, id);
-
-//     // Delete data from S3
-//     await s3Client.send(command);
-
-//     logger.debug({ ownerId, id }, 'Fragment deleted from S3');
-//   } catch (err) {
-//     const { Bucket, Key } = params;
-//     logger.error({ err, Bucket, Key }, 'Error deleting fragment from S3');
-//     throw new Error('unable to delete fragment');
-//   }
-// }
 
 
 
